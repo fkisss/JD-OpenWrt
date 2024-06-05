ae4a5324edf2def98fce0a39f7c9ad3fcd4ca2b4
fkisss <61588117+fkisss@users.noreply.github.com>
2024-06-05 09:35:27 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/messense/aliyundrive-webdav package/messense
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+#以下3行之前有，被j注释掉了
+#git clone https://github.com/messense/aliyundrive-webdav package/messense
+#git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
